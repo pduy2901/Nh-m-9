acc354d8b0aa53588a6b9e0a30c03bbbbd959962
nvlong3 <21013235@st.phenikaa-uni.edu.vn>
2024-02-27 00:12:28 +0700

    vlog2

---
+++ Weather_App-master/lib/service/weather_service.dart
@@ -0,0 +1,62 @@
+import '/api/api_repository.dart';
+import '/model/current_weather_data.dart';
+import '/model/five_days_data.dart';
+
+class WeatherService {
+  final String city;
+
+  String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  String apiKey = 'appid=528f7c7907b2e24b0d433eb025e438b1';
+
+
+  WeatherService({required this.city});
+
+  void getCurrentWeatherData({
+    Function()? beforSend,
+    Function(CurrentWeatherData currentWeatherData)? onSuccess,
+    Function(dynamic error)? onError,
+  }) {
+    final url = '$baseUrl/weather?q=$city&lang=en&$apiKey';
+    ApiRepository(
+      url: '$url',
+    ).get(
+        beforeSend: () => {
+              if (beforSend != null)
+                {
+                  beforSend(),
+                },
+            },
+        onSuccess: (data) => {
+              onSuccess!(CurrentWeatherData.fromJson(data)),
+            },
+        onError: (error) => {
+              if (onError != null)
+                {
+                  print(error),
+                  onError(error),
+                }
+            });
+  }
+
+  void getFiveDaysThreeHoursForcastData({
+    Function()? beforSend,
+    Function(List<FiveDayData> fiveDayData)? onSuccess,
+    Function(dynamic error)? onError,
+  }) {
+    final url = '$baseUrl/forecast?q=$city&lang=en&$apiKey';
+    print(url);
+    ApiRepository(
+      url: '$url',
+    ).get(
+        beforeSend: () => {},
+        onSuccess: (data) => {
+              onSuccess!((data['list'] as List)
+                  .map((t) => FiveDayData.fromJson(t))
+                  .toList())
+            },
+        onError: (error) => {
+              print(error),
+              onError!(error),
+            });
+  }
+}
